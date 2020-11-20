c7041afb7333336e3a1e17ee4188ab4fb2a91d8f
act <act@mediamid.com>
2020-11-20 21:27:38 +0100

    change update time to 10 minutes

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def manageUsers():
 # set a background scheduler
 scheduler = BackgroundScheduler()
 # set a scheduler with interval 3 minute    
-job1 = scheduler.add_job(get_cases_world, 'interval', minutes=0.2)
+job1 = scheduler.add_job(get_cases_world, 'interval', minutes=10)
 # start the scheduler
 scheduler.start()
 
